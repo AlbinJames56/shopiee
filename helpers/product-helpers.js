f85e1f7dc5bb57a5ddb1b2fed112b1bc904b57d0
AlbinJames56 <albinjames56@gmail.com>
2024-03-15 14:26:43 +0530

    added edit button and its functions

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,49 +1,91 @@
-const {db} = require('../config/connection');
-const { connectToDatabase } = require('../config/connection');
-var collection=require('../config/collections')
-var ObjectId=require('mongodb').ObjectId;
+const { db } = require("../config/connection");
+const { connectToDatabase } = require("../config/connection");
+var collection = require("../config/collections");
+var ObjectId = require("mongodb").ObjectId;
 
 module.exports = {
-    addProduct: async (product, callback) => {
-        //console.log(product);
-        try {
-            
-            const database= await connectToDatabase(); 
-            const result = await database.collection(collection.PRODUCT_COLLECTION).insertOne(product);
-            console.log('Product added successfully:', result);
-            const insertedId = result.insertedId.toString(); // Convert ObjectId to string
-            //console.log('Inserted ID:', insertedId);
-            callback(insertedId);
-        } catch (error) {
-            console.error('Error adding product:', error);
-            callback(false);
-        }
-    },
-    getAllProducts:async(req,res)=>{
-        try {
-            
-            const database= await connectToDatabase(); 
-            const products = await database.collection(collection.PRODUCT_COLLECTION).find().toArray();
-            
-            //console.log('readed:', products);
-            //res.render('admin/view-products', { products});
-            return products;
-        } catch (error) {
-            console.error('Error sending product:', error);
-           
-        }
-    },
-    deleteProduct: (prodId) => {
-        return new Promise(async (resolve, reject) => {
-            try {
-                const database = await connectToDatabase();
-                const response = await database.collection(collection.PRODUCT_COLLECTION).deleteOne({ _id: new ObjectId(prodId) });
-                console.log(response);
-                resolve(response);
-            } catch (error) {
-                console.error('Error deleting product:', error);
-                reject(error);
-            }
-        });
+  addProduct: async (product, callback) => {
+    //console.log(product);
+    try {
+      const database = await connectToDatabase();
+      const result = await database
+        .collection(collection.PRODUCT_COLLECTION)
+        .insertOne(product);
+      console.log("Product added successfully:", result);
+      const insertedId = result.insertedId.toString(); // Convert ObjectId to string
+      //console.log('Inserted ID:', insertedId);
+      callback(insertedId);
+    } catch (error) {
+      console.error("Error adding product:", error);
+      callback(false);
     }
+  },
+  getAllProducts: async (req, res) => {
+    try {
+      const database = await connectToDatabase();
+      const products = await database
+        .collection(collection.PRODUCT_COLLECTION)
+        .find()
+        .toArray();
+
+      //console.log('readed:', products);
+      //res.render('admin/view-products', { products});
+      return products;
+    } catch (error) {
+      console.error("Error sending product:", error);
+    }
+  },
+  deleteProduct: (prodId) => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const database = await connectToDatabase();
+        const response = await database
+          .collection(collection.PRODUCT_COLLECTION)
+          .deleteOne({ _id: new ObjectId(prodId) });
+        console.log(response);
+        resolve(response);
+      } catch (error) {
+        console.error("Error deleting product:", error);
+        reject(error);
+      }
+    });
+  },
+  getProductDetails: (prodId) => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const database = await connectToDatabase();
+        const product = await database
+          .collection(collection.PRODUCT_COLLECTION)
+          .findOne({ _id: new ObjectId(prodId) });
+        resolve(product);
+      } catch (error) {
+        console.error("Error editing product:", error);
+        reject(error);
+      }
+    });
+  },
+  updateProduct:(prodId,prodDetails)=>{
+    return new Promise(async (resolve,reject)=>{
+        try {
+            const database = await connectToDatabase();
+            const product = await database
+              .collection(collection.PRODUCT_COLLECTION)
+              .updateOne({ _id: new ObjectId(prodId) },{
+                $set : {
+                    name:prodDetails.name,
+                    category:prodDetails.category,
+                    price: prodDetails.price,
+
+
+              }
+            }).then((response)=>{
+                resolve()
+            })
+          } catch (error) {
+            console.error("Error editing product:", error);
+            reject(error);
+          }
+
+    })
+  }
 };
--- routes/admin.js
+++ routes/admin.js
@@ -1,50 +1,75 @@
-var express = require('express');
+var express = require("express");
 //const {render}=require('../app')
 var router = express.Router();
-var productHelpers=require('../helpers/product-helpers')
+var productHelpers = require("../helpers/product-helpers");
 
 /* GET users listing. */
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const products = await productHelpers.getAllProducts();
-    res.render('admin/view-products', { products, admin: true });
+    res.render("admin/view-products", { products, admin: true });
   } catch (error) {
-    console.error('Error fetching products:', error);
-    res.status(500).send('Internal Server Error');
+    console.error("Error fetching products:", error);
+    res.status(500).send("Internal Server Error");
   }
 });
-router.get('/add-products',(req,res)=>{
+router.get("/add-products", (req, res) => {
   res.render("admin/add-products");
 });
-router.post('/add-product', (req, res) => {
+router.post("/add-product", (req, res) => {
   // console.log(req.body);
   // console.log(req.files.image);
 
   productHelpers.addProduct(req.body, (id) => {
-      if (id) {
-          // Product added successfully, redirect to the add-products page
-          let image=req.files.image
-          console.log(id)
-          image.mv('./public/product-images/'+id+'.jpg', function(err) {
-            if (err) return res.status(500).send(err);
-          else res.redirect('/admin/add-products');
-      })} else {
-          // Product addition failed, render the add-products page with an error message
-          res.render('admin/add-products', { error: 'Failed to add product' });
-      }
+    if (id) {
+      // Product added successfully, redirect to the add-products page
+      let image = req.files.image;
+      console.log(id);
+      image.mv("./public/product-images/" + id + ".jpg", function (err) {
+        if (err) return res.status(500).send(err);
+        else res.redirect("/admin/add-products");
+      });
+    } else {
+      // Product addition failed, render the add-products page with an error message
+      res.render("admin/add-products", { error: "Failed to add product" });
+    }
   });
 });
-router.get('/delete-product/:id',(req,res)=>{
-  let prodId=req.params.id;
+router.get("/delete-product/:id", (req, res) => {
+  let prodId = req.params.id;
   if (prodId.length !== 24) {
-    return res.status(400).send('Invalid product ID');
-}
-productHelpers.deleteProduct(prodId).then((response) => {
-    res.redirect('/admin/');
-}).catch((error) => {
-    console.error('Error deleting product:', error);
-    res.redirect('/admin/');
+    return res.status(400).send("Invalid product ID");
+  }
+  productHelpers
+    .deleteProduct(prodId)
+    .then((response) => {
+      res.redirect("/admin/");
+    })
+    .catch((error) => {
+      console.error("Error deleting product:", error);
+      res.redirect("/admin/");
+    });
 });
+router.get("/edit-product/:id", async (req, res) => {
+  try {
+    let product = await productHelpers.getProductDetails(req.params.id);
+    console.log("to edit", product);
+    res.render("admin/edit-product", { product });
+  } catch (error) {
+    console.error("Error editing product:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+router.post('/edit-product/:id',(req,res)=>{
+  let id=req.params.id;
+  productHelpers.updateProduct(id,req.body).then(()=>{
+    res.redirect(303,"/admin");
+    if(req.files.image){
+let image=req.files.image;
+      image.mv("./public/product-images/" + id + ".jpg");
+
+    }
+  })
 })
- 
+
 module.exports = router;
